4104cb79f98aa04ff0005724679d834cf625c655
Yimmy Alexander Diaz <Yimy Alexander Diaz@DESKTOP-DSLPE8P>
2021-04-25 16:49:42 -0500

    cambios sobre login

--- MoviePhile_Frontend/lib/main.dart
+++ MoviePhile_Frontend/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_demo/src/pages/Principal.dart';
 //import 'package:flutter_demo/src/pages/alert_page.dart';
@@ -5,7 +7,19 @@ import 'package:flutter_demo/src/pages/Principal.dart';
 import 'package:flutter_demo/src/routes/route.dart';
 //import 'package:flutter_demo/src/pages/home_temp.dart';
 
-void main() => runApp(MyApp());
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
+void main() {
+  HttpOverrides.global = new MyHttpOverrides();
+  runApp(MyApp());
+}
 
 //main principal
 class MyApp extends StatelessWidget {
--- MoviePhile_Frontend/lib/src/pages/LoginUsuario_page.dart
+++ MoviePhile_Frontend/lib/src/pages/LoginUsuario_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/src/Model/Login_Usuario.dart';
+import 'package:flutter_demo/src/services/user_service.dart';
 
 class LoginUsuarioPage extends StatefulWidget {
   @override
@@ -8,8 +10,10 @@ class LoginUsuarioPage extends StatefulWidget {
 // Estructura de la pagina login
 // Con sus respectivos campos de email y password
 class _LoginPageState extends State<LoginUsuarioPage> {
-  String _email = '';
-  String _password = '';
+  UserLogin userLogin;
+  final emailCotroler = TextEditingController();
+  final passwordControler = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,14 +34,20 @@ class _LoginPageState extends State<LoginUsuarioPage> {
           Divider(),
           _crearContrasena(),
           Divider(),
-          _crearPersona(),
           RaisedButton(
-            child: Text('Ingresar'),
+            child: Text('Login Usuario'),
             color: Colors.blue,
             textColor: Colors.white,
             shape: StadiumBorder(),
             onPressed: () {
-              _mensaje(context);
+              userLogin = UserLogin(
+                  email: emailCotroler.text, password: passwordControler.text);
+              getToken(userLogin).then((value) => {
+                    if (value != null)
+                      {Navigator.pushNamed(context, '/')}
+                    else
+                      {_mostrarAlerta(context)}
+                  });
             },
           ),
         ],
@@ -50,18 +60,18 @@ class _LoginPageState extends State<LoginUsuarioPage> {
   Widget _crearEmail() {
     return TextField(
       //autofocus: true,
+      controller: emailCotroler,
       keyboardType: TextInputType.emailAddress,
+      style: TextStyle(
+        color: Colors.black,
+        fontFamily: 'OpenSans',
+      ),
       decoration: InputDecoration(
           border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
           hintText: 'Email',
           labelText: 'Email',
           suffixIcon: Icon(Icons.alternate_email),
           icon: Icon(Icons.email)),
-      onChanged: (valor) {
-        setState(() {
-          _email = valor;
-        });
-      },
     );
   }
 
@@ -69,20 +79,20 @@ class _LoginPageState extends State<LoginUsuarioPage> {
 // return el campo de ingresar password
   Widget _crearContrasena() {
     return TextField(
-      //autofocus: true,
-      obscureText: true,
-      decoration: InputDecoration(
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
-          hintText: 'Password',
-          labelText: 'Password',
-          suffixIcon: Icon(Icons.lock_open),
-          icon: Icon(Icons.lock)),
-      onChanged: (valor) {
-        setState(() {
-          _password = valor;
-        });
-      },
-    );
+        //autofocus: true,
+        controller: passwordControler,
+        obscureText: true,
+        style: TextStyle(
+          color: Colors.black,
+          fontFamily: 'OpenSans',
+        ),
+        decoration: InputDecoration(
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+            hintText: 'Password',
+            labelText: 'Password',
+            suffixIcon: Icon(Icons.lock_open),
+            icon: Icon(Icons.lock)));
   }
 
 // Metodo que estructura al momento de dar un click con una alerta
@@ -95,11 +105,11 @@ class _LoginPageState extends State<LoginUsuarioPage> {
         return AlertDialog(
           shape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
-          title: Text('Datos Faltantes'),
+          title: Text('Error'),
           content: Column(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              Text('Llenar campos vacios'),
+              Text('Datos faltantes'),
               // FlutterLogo(size: 100.0)
             ],
           ),
@@ -108,25 +118,11 @@ class _LoginPageState extends State<LoginUsuarioPage> {
                 child: Text('OK'),
                 onPressed: () {
                   // Navigator.pop(context);
-                  Navigator.pushNamed(context, 'avatar');
+                  Navigator.pushNamed(context, '/');
                 })
           ],
         );
       },
     );
-  }
-
-  Widget _crearPersona() {
-    return ListTile(
-      title: Text('Nombre es: $_email' + ' Apellido es $_password'),
-    );
-  }
-
-  void _mensaje(BuildContext context) {
-    if ('$_email' != null && '$_password' != null) {
-      _mostrarAlerta(context);
-    } else {
-      print('entro,,,ksajdkjask');
-    }
   }
 }
