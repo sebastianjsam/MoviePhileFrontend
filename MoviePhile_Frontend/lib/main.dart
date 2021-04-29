a2609132c4f4e6a621214e3c10aa9044cc56bc25
Yimmy Alexander Diaz <Yimy Alexander Diaz@DESKTOP-DSLPE8P>
2021-04-29 00:13:48 -0500

    Cambios generales

--- MoviePhile_Frontend/lib/main.dart
+++ MoviePhile_Frontend/lib/main.dart
@@ -21,14 +21,14 @@ void main() {
   runApp(MyApp());
 }
 
-class MyHttpOverrides extends HttpOverrides {
+/*class MyHttpOverrides extends HttpOverrides {
   @override
   HttpClient createHttpClient(SecurityContext context) {
     return super.createHttpClient(context)
       ..badCertificateCallback =
           (X509Certificate cert, String host, int port) => true;
   }
-}
+}*/
 
 //main principal
 class MyApp extends StatelessWidget {
